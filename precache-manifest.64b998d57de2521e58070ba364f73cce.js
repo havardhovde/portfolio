642bb38a80d6e2f606fd804b2a056b17f060fe5e
havardhovde <haavardhovde@gmail.com>
2020-02-05 15:03:24 +0100

    Updates

---
+++ precache-manifest.64b998d57de2521e58070ba364f73cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6679ac49c3a9a9dd74bde05394e066c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8c751e54020080e27180",
+    "url": "/portfolio/static/css/main.a5792fd0.chunk.css"
+  },
+  {
+    "revision": "1324061f0add06a61b0b",
+    "url": "/portfolio/static/js/2.632ddaeb.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/portfolio/static/js/2.632ddaeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c751e54020080e27180",
+    "url": "/portfolio/static/js/main.fcb48cdc.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
